0e2e3e1ef9d04ba466489f28d8ec3975a9955bdb
matsurai25 <m.matsurai25@gmail.com>
2017-03-12 23:04:23 +0900

    scopeにdefaultを指定
    user_pathのように引数一つだけ渡せるものの挙動がおかしくなっていた

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope '(:locale)', constraints: { locale: /\w{2}/ } do
+  scope '(:locale)', constraints: { locale: /ja|en/ }, defaults: {locale: 'en'} do
     root 'landing#index'
     resources :users
     resources :products
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
         resources :users
       end
     end
-    get  '*unmatched_route', to: 'errors#render_404', format: false
+    # get  '*unmatched_route', to: 'errors#render_404', format: false
   end
 end
