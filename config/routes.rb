d5395ddfcc73b08ebc57a94f3c268244ef835eab
matsurai25 <m.matsurai25@gmail.com>
2017-03-13 01:03:23 +0900

    404を出す

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
         resources :users
       end
     end
-    # get  '*unmatched_route', to: 'errors#render_404', format: false
+    get  '*unmatched_route', to: 'errors#render_404', format: false
   end
 end
